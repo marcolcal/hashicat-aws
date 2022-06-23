50a1358f311609af5a268c0d787375209f1ef2a8
MArco Alfaro <marcolcal@gmail.com>
2022-06-23 20:24:55 +0000

    Fix tags

--- main.tf
+++ main.tf
@@ -128,12 +128,8 @@ resource "aws_instance" "hashicat" {
 
   tags = {
     Name = "${var.prefix}-hashicat-instance"
-  }
-  tags = {
     Name = "devops"
-  }
-  tags = {
-    Name = "Billable "
+    Name = "Billable"
   }
 }
 
