31ab64bb8ed47e727200cc9861636aa7089f8421
MArco Alfaro <marcolcal@gmail.com>
2022-06-23 20:31:25 +0000

    Fix tags 3

--- variables.tf
+++ variables.tf
@@ -7,7 +7,7 @@
 
 variable "prefix" {
   description = "This prefix will be included in the name of most resources."
-  default = "Marco Alfaro"
+  default = "Marco-Alfaro"
 }
 
 variable "region" {
